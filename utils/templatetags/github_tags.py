935586c6ed2457e7587c6c06015699ec322a4a9f
Zbigniew Siciarz <antyqjon@gmail.com>
2012-07-10 19:20:34 +0200

    Added latest Git commit again.

---
+++ utils/templatetags/github_tags.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Zbigniew Siciarz 2010-2012.
+
+from django import template
+
+from github import Github
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("commit.html")
+def last_commit(username, repository_name):
+    u"""
+    Returns last Git commit for a given repository.
+    """
+    try:
+        g = Github()
+        user = g.get_user(username)
+        repo = user.get_repo(repository_name)
+        commits = repo.get_commits()
+        return {'commit': commits[0]}
+    except Exception:
+        return {'commit': None}
