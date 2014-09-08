1c2306a591e56635378b96c5ec0a38a4e58430d0
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-08 19:52:40 +0200

    Replaced South migrations with Django native ones.

--- articles/migrations/0001_initial.py
+++ articles/migrations/0001_initial.py
@@ -1,44 +1,36 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import model_utils.fields
+import taggit.managers
+import markitup.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Article'
-        db.create_table('articles_article', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
-            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255)),
-            ('content', self.gf('markupfield.fields.MarkupField')(rendered_field=True)),
-            ('content_markup_type', self.gf('django.db.models.fields.CharField')(default='markdown', max_length=30)),
-            ('_content_rendered', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('articles', ['Article'])
+    dependencies = [
+        ('taggit', '0002_auto_20140908_1949'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Article'
-        db.delete_table('articles_article')
-
-
-    models = {
-        'articles.article': {
-            'Meta': {'ordering': "['-created']", 'object_name': 'Article'},
-            '_content_rendered': ('django.db.models.fields.TextField', [], {}),
-            'content': ('markupfield.fields.MarkupField', [], {'rendered_field': 'True'}),
-            'content_markup_type': ('django.db.models.fields.CharField', [], {'default': "'markdown'", 'max_length': '30'}),
-            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['articles']
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('title', models.CharField(max_length=255, verbose_name='title')),
+                ('slug', models.SlugField(unique=True, max_length=255, verbose_name='slug')),
+                ('content', markitup.fields.MarkupField(verbose_name='content')),
+                ('_content_rendered', models.TextField(editable=False, blank=True)),
+                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+            ],
+            options={
+                'ordering': ['-created'],
+                'verbose_name_plural': 'Articles',
+            },
+            bases=(models.Model,),
+        ),
+    ]
