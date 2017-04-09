d8e681fb3dffd2472beb4d2d98945ec40a499f95
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-04-09 21:56:23 +0200

    Modernize admin URLconf.

--- aquila/urls.py
+++ aquila/urls.py
@@ -71,7 +71,7 @@ urlpatterns = [
     ),
     url(r'^articles/', include('articles.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^markitup/', include('markitup.urls')),
 ]
